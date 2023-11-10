11d88d6d159aa2023079bd1aff77e1e8053ff72e
PrashanthJ11 <jangamprashanthjaya@gmail.com>
2023-11-10 10:23:11 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Rishi(request):
+    return render(request,'Rishi.html')
+
+def Harrish(request):
+    return render(request,'Harrish.html')
+
+def Vishnu(request):
+    return render(request,'Vishnu.html')
+
+def Arun(request):
+    return render(request,'Arun.html')
